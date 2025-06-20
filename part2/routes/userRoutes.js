ae81bce2760e6d19d8845e66251502e5bf49ac32
sober449 <a1917096@adelaide.edu.au>
2025-06-20 16:40:06 +0000

    [automatic commit] 2025-06-20 16:40:06 UTC 120.20.135.239

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,7 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// POST login
+// POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body; // use username password
 
